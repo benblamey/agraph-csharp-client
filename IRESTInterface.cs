abf909d7d10e20c588cf03425f1a61fb15b7d6d9
Ben Blamey <ben@ml4d.com>
2012-06-14 13:20:49 +0100

    Build XML documentation for Release build. Sort usings.

--- IRESTInterface.cs
+++ IRESTInterface.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ServiceModel;
-using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Web;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
 namespace AllegroGraphCSharpClient
 {
     [ServiceContract]
--- Request.cs
+++ Request.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ServiceModel;
-using ServiceModelEx;
-using System.Runtime.Serialization;
-using System.Net;
 using System.IO;
+using System.Net;
 
 
 namespace AllegroGraphCSharpClient
